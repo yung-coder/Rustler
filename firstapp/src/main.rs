bebd0ef668f717aacfe9da589be76f8ca2fd0483
yung-coder <chandel.sanjeev5@gmail.com>
2023-05-31 23:34:18 +0530

    Rustling with first app

--- firstapp/src/main.rs
+++ firstapp/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -14,10 +15,18 @@ fn main() {
     println!("Searching for  {}", config.query);
     println!("Filename is {}", config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading a file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
