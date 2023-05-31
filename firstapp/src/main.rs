8d8b69ead9728d3a4d1b367efd7c6d6dafa8bc61
yung-coder <chandel.sanjeev5@gmail.com>
2023-05-31 23:29:29 +0530

    Rustling with first app

--- firstapp/src/main.rs
+++ firstapp/src/main.rs
@@ -1,16 +1,38 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Args {:?}", args);
-    println!("Searching for  {}", query);
-    println!("Filename is {}", filename);
+    println!("Searching for  {}", config.query);
+    println!("Filename is {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading a file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading a file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
