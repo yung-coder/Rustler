3ec77105d8382e70b5e965884930ea64010c9f69
yung-coder <chandel.sanjeev5@gmail.com>
2023-06-02 23:35:53 +0530

    Rustling with improving

--- firstapp/src/main.rs
+++ firstapp/src/main.rs
@@ -4,16 +4,10 @@ use std::process;
 use firstapp::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+    let config: Config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("Args {:?}", args);
-    println!("Searching for  {}", config.query);
-    println!("Filename is {}", config.filename);
 
     if let Err(e) = firstapp::run(config) {
         eprintln!("Application error: {}", e);
