738766e6e3dac42cb97c14258b28bd26d409bf4d
yung-coder <chandel.sanjeev5@gmail.com>
2023-06-08 22:20:05 +0530

    Rustling with Concurrency

---
+++ UnderstandingRust/src/Conc.rs
@@ -0,0 +1,18 @@
+use std::{thread, time::Duration};
+
+fn main() {
+    let handel = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from spawned", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} for main", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // blocking  main thread tiil complete
+    handel.join().unwrap();
+}
