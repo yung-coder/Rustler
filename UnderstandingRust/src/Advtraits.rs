a9b3b1222bbd668580f71e874b3db989a4f99137
yung-coder <chandel.sanjeev5@gmail.com>
2023-06-24 22:41:40 +0530

    Rustling with life

--- UnderstandingRust/src/Advtraits.rs
+++ UnderstandingRust/src/Advtraits.rs
@@ -19,7 +19,7 @@ impl Add for Point {
 
 fn main() {
     assert_eq!(
-        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
-        Point { x: 3, y: 3 }
+        Point { x: 1, y: 1 } + Point { x: 1, y: 1 },
+        Point { x: 2, y: 2 }
     );
 }
