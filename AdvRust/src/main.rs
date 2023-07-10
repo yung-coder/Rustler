7bb6679bc599204128a0d75739d7a0f41a6d57da
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-10 23:06:29 +0530

    rustling

--- AdvRust/src/main.rs
+++ AdvRust/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Adv rust keep low  priority");
+    let (mut x, y) = (1, 2);
+    x += 2;
+
+    assert_eq!(x, 3);
+    assert_eq!(y, 2);
+
+    println!("Done");
+
+    let z;
+
+    [.., z] = [1, 2];
+    assert_eq!(z, 2);
+
+    println!("Done this too");
 }
