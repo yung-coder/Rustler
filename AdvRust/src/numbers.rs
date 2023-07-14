2a84042ee88045274c7822586aeafe560ea46a0f
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-14 14:23:05 +0530

    Returning syntax

--- AdvRust/src/numbers.rs
+++ AdvRust/src/numbers.rs
@@ -6,4 +6,19 @@ fn main() {
     for c in 'a'..='z' {
         println!("{}", c as u8);
     }
+
+    let t = true;
+    if !t {
+        println!("Test");
+    }
+
+    let v = {
+        let mut x: i32 = 1;
+        x += 2;
+        x
+    };
+
+    assert_eq!(v, 3);
+
+    println!("Done!");
 }
