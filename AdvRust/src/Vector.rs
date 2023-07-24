c86bd0b95b065bc933248b798d6cfd671496c35d
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-25 00:24:19 +0530

    Vectors

--- AdvRust/src/Vector.rs
+++ AdvRust/src/Vector.rs
@@ -9,4 +9,20 @@ fn main() {
     assert_eq!(v1, v2);
 
     println!("Done");
+
+    let mut v = vec![1, 2, 3];
+
+    let slice1 = &v[..];
+
+    let slice2 = &v[0..v.len()];
+
+    assert_eq!(slice1, slice2);
+
+    let vec_ref = &mut v;
+    (*vec_ref).push(4);
+    let slice3 = &v[0..];
+
+    assert_eq!(slice3, &[1, 2, 3, 4]);
+
+    println!("Done too");
 }
