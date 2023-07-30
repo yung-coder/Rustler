41fcefed844368430291994efda72b0c70869394
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-30 23:34:32 +0530

    lifetimes

--- AdvRust/src/Lifetimes.rs
+++ AdvRust/src/Lifetimes.rs
@@ -1,1 +1,24 @@
-fn main() {}
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    {
+        let r;
+        {
+            let x = 5;
+            r = &x;
+        }
+        // will not compile as  x can not be accessed outside
+        println!("r:{}", r);
+    }
+
+    let x = "long";
+    let y = "longer";
+
+    println!("{}", longest(x, y));
+}
