f899eef9940cb76d027840b31978a7ca1fe595d0
Saksham Chandel <chandel.sanjeev5@gmail.com>
2024-06-17 21:12:07 +0530

    iterations

--- Rustler2/concepts/src/main.rs
+++ Rustler2/concepts/src/main.rs
@@ -6,5 +6,32 @@ fn main() {
 
 
     // print!("{}", _ax.chars().nth(0)) 
+
+
+    // basic loops
+    // 0 to 9 
+    for i in 0..10 {
+        print!("{}", i);
+    }
+
+    /// for iteration of strings 
     
+    let sen = String::from("Lets see rust");
+    let first = get_first(sen); 
+    print!("First word is: {}", first);
+
+}
+
+
+// complexity lies underround 
+
+fn get_first(sen: String) -> String {
+    let mut ans = String::from("");
+    for char  in sen.chars() {
+        ans.push_str(char.to_string().as_str());
+        if char == ' '{
+            break;
+        }
+    }
+    return  ans;
 }
