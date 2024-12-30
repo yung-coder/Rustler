7339c877b2770cb70c6c6cea28964b7891fccc78
Saksham Chandel <chandel.sanjeev5@gmail.com>
2024-12-31 00:29:39 +0530

    moving and borrowing

--- Rustler2/concepts/src/complex.rs
+++ Rustler2/concepts/src/complex.rs
@@ -1,2 +1,31 @@
 // 1 Memory theory ( NO CODE )  (done)
 // Mutablity (done)
+
+// Moving 
+
+fn main(){
+    let s1 = String::from("Rusty");
+    do_some(s2:s1);
+    println!("number {}", s1); // --> not valid s1 is moved to the function
+}
+
+
+fn do_some(s2: string) {
+    println!("{}" , s2)
+}
+
+// Borrowing 
+
+
+fn main(){
+    let s1 = String::from("Rusty");
+    do_some(s2:&s1); // --> borrowing the s1 
+    println!("number {}", s1); // --> not valid s1 is moved to the function
+}
+
+
+fn do_some(s2: &string) {
+    println!("{}" , s2)
+}
+
+// At any given time you can have either one mutable refernce or any number of immutabl references 
