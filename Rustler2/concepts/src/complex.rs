16988b58f11167ac208b8ec56dfaffc21665f45d
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-01-02 13:48:49 +0530

    vectors done

--- Rustler2/concepts/src/complex.rs
+++ Rustler2/concepts/src/complex.rs
@@ -35,8 +35,20 @@ fn do_some(s2: &string) {
 
 let main() {
     let  mut vec = Vec::new();
+    let mut vec2 = vec![1,2,3,4]
     vec.push(1);
     vec.push(2);
 
     println!("{:?}", vec);
+}
+
+fn even(vec: Vec<i32>) -> Vec<i32> {
+    let mut new_vec = Vec::new();
+    for val in vec {
+        if val % 2 == 0 {
+            new_vec.push(val);
+        }
+    }
+
+    return new_vec
 }
