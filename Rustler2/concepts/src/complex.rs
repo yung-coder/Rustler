0e871297d6bc975b18382787042d6a70c69c3cff
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-01-02 23:34:37 +0530

    traits

--- Rustler2/concepts/src/complex.rs
+++ Rustler2/concepts/src/complex.rs
@@ -169,3 +169,28 @@ fn find_first(word: String) -> &str {
 
     return &word[0..index];
 }
+
+// traits 
+
+trait Summary {
+    fn summarize(&self) -> String;
+}
+
+struct User {
+    name: String,
+    age: u32,
+}
+
+impl Summary for User {
+    fn summarize(&self) -> String {
+        return format!("The name is {}, and the age is{}", self.name, self.age);
+    }
+}
+
+fn main() {
+    let user = User {
+        name: String::from("Saksham");
+        age: 21,
+    };
+    println!("{}", user.summarize());
+}
