c729776fad5f476fcad0bcf70911a5e4c4f4e84f
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-01-03 23:24:13 +0530

    lifetimes done

--- Rustler2/concepts/src/complex.rs
+++ Rustler2/concepts/src/complex.rs
@@ -194,3 +194,24 @@ fn main() {
     };
     println!("{}", user.summarize());
 }
+
+// lifetimes
+
+fn main(){
+    let ans;
+
+    let str1 = String::from("aa");
+    {
+        let srt2 = String::from("sadsadasda");
+
+        ans = longest(&str1, &str2);
+    }
+}
+
+fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
+    if str1.len() > str2.len() {
+        return str1;
+    } else {
+        return str2;
+    }
+}
