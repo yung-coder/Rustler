49bc5857fa9ff53e52121ee733231cc7188106c0
Saksham Chandel <chandel.sanjeev5@gmail.com>
2025-01-04 00:03:07 +0530

    message passing done

--- Rustler2/concepts/src/complex.rs
+++ Rustler2/concepts/src/complex.rs
@@ -238,3 +238,15 @@ fn main() {
         }
     }
 }
+
+// message passing 
+
+fn main(){
+    let (tx, rx) = mpsc::channel();
+    spawn(move || {
+        tx.send(String::from("Check")).unwrap();
+    });
+
+    let value = rx.recv().unwrap();
+    println!("{}", value);
+}
